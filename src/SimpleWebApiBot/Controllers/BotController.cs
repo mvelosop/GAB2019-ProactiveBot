822df8787c2fa0ff037322ac74988175430d43d7
Miguel Veloso <mvelosop@gmail.com>
2019-04-17 20:02:18 +0100

    Simple web API Bot

---
+++ src/SimpleWebApiBot/Controllers/BotController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleWebApiBot.Controllers
+{
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly ILogger<BotController> _logger;
+        private readonly IAdapterIntegration _adapter;
+
+        public BotController(
+            ILogger<BotController> logger,
+            IAdapterIntegration adapter)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _adapter = adapter ?? throw new ArgumentNullException(nameof(adapter));
+        }
+
+        [HttpPost("/simple-bot/messages")]
+        public async Task<InvokeResponse> Messages([FromBody]Activity activity)
+        {
+            _logger.LogTrace("----- BotController - Receiving activity: {@Activity}", activity);
+
+            return await _adapter.ProcessActivityAsync(string.Empty, activity, OnTurnAsync, default);
+        }
+
+        private async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var text = turnContext.Activity.Text.Trim();
+
+                _logger.LogInformation("----- Receiving message activity - Text: {Text}", text);
+
+                // Echo back to the user whatever they typed.
+                await turnContext.SendActivityAsync($"You typed \"{text}\"");
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+}
